6fc9086687cb1351f45362e704f4db29bf39c195
NIKIETA <nikieta09@gmail.com>
2024-09-09 11:59:05 +0530

    Update 2-counter.js

--- 01-async-js/easy/2-counter.js
+++ 01-async-js/easy/2-counter.js
@@ -2,17 +2,13 @@
 
 // Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
 var n=0;
-function callback(count,duration){
-  setTimeout(count,duration);
-}
+
 function count(){
   console.log(n);
   n++;
-  callback(count,1000);
+  setTimeout(count,1000);
 }
 count();
-
-
 
 
 
