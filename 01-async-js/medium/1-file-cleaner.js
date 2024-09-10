fd2de973e01b94626bbaa8d9df28697a57a7cdeb
NIKIETA <nikieta09@gmail.com>
2024-09-10 19:20:34 +0530

    Update 1-file-cleaner.js

--- 01-async-js/medium/1-file-cleaner.js
+++ 01-async-js/medium/1-file-cleaner.js
@@ -12,24 +12,40 @@
 // hello world my name is raman
 // ```
 
-const fs=require('fs');
-const filePath='a.txt';
 
-
-fs.readFile(filePath,'utf-8',function(err,data){
+const fs=require("fs")
+fs.readFile("a.txt","utf-8",function(err,data){
   if(err){
-    console.log('Error reading the file,err');
-    return;
+    console.log(err);
+  }
+  else{
+
+    console.log("New data is ",data.replace(/\s+/g,' '));
+    
   }
 
-var data2=data.replace(/\s+/g, ' ');
-console.log('File content: ',data);
-});
+})
 
-fs.readFile(filePath,'utf-8',function(err,data){
-if(err){
-  console.log('Error reading the file,err');
-  return;
-}
-  console.log("Final file content: ",data.replace(/\s+/g, ' '));;
-});
+
+// Method 2
+// const fs=require('fs');
+// const filePath='a.txt';
+
+
+// fs.readFile(filePath,'utf-8',function(err,data){
+//   if(err){
+//     console.log('Error reading the file,err');
+//     return;
+//   }
+
+// var data2=data.replace(/\s+/g, ' ');
+// console.log('File content: ',data);
+// });
+
+// fs.readFile(filePath,'utf-8',function(err,data){
+// if(err){
+//   console.log('Error reading the file,err');
+//   return;
+// }
+//   console.log("Final file content: ",data.replace(/\s+/g, ' '));;
+// });
